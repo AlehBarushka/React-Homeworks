71e602d699acbec5a9eb5312db97aafeb567bd99
AlehBarushka <1.AlehBarushka@gmail.com>
2021-12-04 13:16:28 +0300

    renamed wokr1 and created work2

--- my-app/src/App.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import Button from './Components/Button';
-
-function App(props) {
-	return <Button handleClick={ props.handleClick } handleMouseEnter={ props.handleMouseEnter } name={ props.buttonNames.selfdestructButton } />;
-}
-
-export default App;
--- my-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import buttonNames, { handleClick, handleMouseEnter } from './state';
-
-ReactDOM.render(<App buttonNames={ buttonNames } handleClick={ handleClick } handleMouseEnter={ handleMouseEnter } />, document.getElementById('root'));
